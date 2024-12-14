227f056d08b18bffab7fa9e978fd2a95889a8016
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-12-14 19:40:04 +0900

    Feat : 카테고리 기능 개발

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,34 +1,29 @@
-import { useState } from "react";
 import styled from "styled-components";
 
 interface CategoryInfo {
   text: string;
   onClick?: () => void;
+  isActive: boolean;
 }
 
-const Layout = styled.button<{ isClicked: boolean }>`
+const Layout = styled.button<{ isActive: boolean }>`
   padding: 10px 14px;
-  background-color: ${({ isClicked }) => (isClicked ? "black" : "#a9a9a9")};
+  background-color: ${({ isActive }) => (isActive ? "black" : "#a9a9a9")};
   border-radius: 10px;
+  color: white;
+  font-size: 14px;
   &:focus {
     outline: none;
   }
 `;
 
 const Title = styled.span`
-  color: white;
   font-size: 14px;
 `;
 
-const Category = ({ text, onClick }: CategoryInfo) => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-    if (onClick) onClick();
-  };
+const Category = ({ text, onClick, isActive }: CategoryInfo) => {
   return (
-    <Layout isClicked={isClicked} onClick={handleClick}>
+    <Layout isActive={isActive} onClick={onClick}>
       <Title>{text}</Title>
     </Layout>
   );
